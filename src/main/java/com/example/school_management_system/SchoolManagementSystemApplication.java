6435dc1b541123a9bff17ea283315764056450a4
mehdi800wa@gmail.com <mehdi800wa@gmail.com>
2024-12-06 04:08:02 -0800

    update 6

--- src/main/java/com/example/school_management_system/SchoolManagementSystemApplication.java
+++ src/main/java/com/example/school_management_system/SchoolManagementSystemApplication.java
@@ -1,9 +1,18 @@
 package com.example.school_management_system;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "School Management System API",
+                version = "1.0",
+                description = "Admin : mehdi@gmail.com  n/ ///////// Password : mehdi"
+        )
+)
 public class SchoolManagementSystemApplication {
 
     public static void main(String[] args) {
